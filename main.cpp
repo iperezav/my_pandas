ee36312b5887d66a11c1363a41961e19cedf8100
Ivan Perez Avellaneda <iperezavellaneda@gmail.com>
2025-08-31 02:55:18 -0400

    Working on one array and finding the minimum and maximum

--- main.cpp
+++ main.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    int first_array[3] = {1,2,3};
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it
+    int first_array[10] = {4,2,5,2,3,4,1,4,5,2};
+    int bounds[2] = {first_array[0],first_array[0]};
+    // count elements
 
-    for (int i = 0; i < 3; i++) {
-        cout << first_array[i];
+    //find the min and max
+    for (int i = 0; i < 10; i++) {
+        if (bounds[0] > first_array[i]) {
+            bounds[0] = first_array[i];
+        }
+        if (bounds[1] < first_array[i]) {
+            bounds[1] = first_array[i];
+        }
     }
+
+    cout << "The minimum value of the array: " << bounds[0] << endl;
+    cout << "The maximum value of the array: " << bounds[1] << endl;
+
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
